e191316575afd2a96f660fa487c2569e0ff2d671
amria <atelieramria@gmail.com>
2025-03-23 09:25:37 -0400

    Added retry, README.md, LICENSE

---
+++ README.md
@@ -0,0 +1,7 @@
+# TES3MP Healthcheck
+
+TES3MP Healthcheck is a utility for monitoring the status of a TES3MP server.  It uses Discord webhooks to send messages when the server goes down and when it comes back up.
+
+See `.env.sample` for an example of which environment variables need to be populated.
+
+This has been developed with Python 3.11.
--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import socket
 import struct
 import time
 
-TIMEOUT = 2
-
-def send_raknet_ping(address, port):
+def send_raknet_ping(address, port, timeout, max_retries, retry_delay):
     RAKNET_MAGIC = b'\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78'
     timestamp = int(time.time() * 1000)
     ping_packet = b'\x01' + struct.pack('>Q', timestamp) + RAKNET_MAGIC
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(TIMEOUT)
+    sock.settimeout(timeout)
 
-    try:
-        sock.sendto(ping_packet, (address, port))
-        response, server = sock.recvfrom(2048)
+    for attempt in range(max_retries):
+        try:
+            sock.sendto(ping_packet, (address, port))
+            response, server = sock.recvfrom(2048)
 
-        if response and response[0] == 0x1C:
-            return True
-        else:
+            if response and response[0] == 0x1C:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            if attempt < max_retries - 1:
+                time.sleep(retry_delay)
             return False
-    except socket.timeout:
-        return False
-    finally:
-        sock.close()
+        finally:
+            sock.close()
 
 def discord_alert(webhook_url, status):
     if status == True:
@@ -41,18 +43,19 @@ def log_status(status):
     else:
         print('Server is down!')
 
-# Code starts
-
-
 def main():
     server_host = os.getenv("server_host")
     server_port = int(os.getenv("server_port"))
     healthcheck_interval = int(os.getenv("healthcheck_interval"))
     discord_webhook_url = os.getenv("discord_webhook_url")
+    timeout = int(os.getenv("timeout"))
+    max_retries = int(os.getenv("max_retries"))
+    retry_delay = int(os.getenv("retry_delay"))
+    
     past_status = True
 
     while True:
-        current_status = send_raknet_ping(server_host, server_port)
+        current_status = send_raknet_ping(server_host, server_port, timeout, max_retries, retry_delay)
 
         if past_status != current_status:
             discord_alert(discord_webhook_url, current_status)
